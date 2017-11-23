d1805d48ba9cb023119eac88594e085a55c8a82e
Dave Shiga <dshiga@broadinstitute.org>
2017-11-22 23:41:33 -0500

    Add code to make cromwell dependencies zip

--- cromwell_tools/cromwell_tools.py
+++ cromwell_tools/cromwell_tools.py
@@ -2,6 +2,10 @@
 """
 import requests
 from requests.auth import HTTPBasicAuth
+import io
+import requests
+import zipfile
+from StringIO import StringIO
 
 
 def start_workflow(wdl_file, zip_file, inputs_file, inputs_file2, options_file, url, user=None, password=None):
@@ -28,3 +32,61 @@ def start_workflow(wdl_file, zip_file, inputs_file, inputs_file2, options_file, 
     response = requests.post(url, files=files, auth=auth)
 
     return response
+
+
+def download_to_map(urls):
+    """
+    Reads contents from each url into memory and returns a
+    map of urls to their contents
+    """
+    url_to_contents = {}
+    for url in urls:
+        contents = download(url)
+        url_to_contents[url] = contents
+    return url_to_contents
+
+
+def make_zip_in_memory(url_to_contents):
+    """
+    Given a map of urls and their contents, returns an in-memory zip file
+    containing each file. For each url, the part after the last slash is used
+    as the file name when writing to the zip archive.
+    """
+    buf = StringIO()
+    with zipfile.ZipFile(buf, 'w') as zip_buffer:
+        for url, contents in url_to_contents.items():
+            name = url.split('/')[-1]
+            zip_buffer.writestr(name, contents)
+
+    bytes_buf = io.BytesIO(buf.getvalue())
+    return bytes_buf
+
+
+def download(url):
+    """
+    Reads the contents located at the url into memory and returns them.
+    Urls starting with http are fetched with an http request. All others are
+    assumed to be local file paths and read from the local file system.
+    """
+    if url.startswith('http'):
+        return download_http(url)
+    else:
+        return read_local_file(url)
+
+
+def download_http(url):
+    """
+    Makes an http request for the contents at the given url and returns the response body.
+    """
+    response = requests.get(url)
+    response_str = response.text.encode('utf-8')
+    return response_str
+
+
+def read_local_file(path):
+    """
+    Reads the file contents and returns them.
+    """
+    with open(path) as f:
+        contents = f.read()
+    return contents
--- cromwell_tools/tests/test_cromwell_tools.py
+++ cromwell_tools/tests/test_cromwell_tools.py
@@ -9,11 +9,18 @@ try:
 except ImportError:
     # if python2
     import mock
-
 from cromwell_tools import cromwell_tools
+import zipfile
+import os
 
 
 class TestUtils(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Change to test directory, as tests may have been invoked from another dir
+        dir = os.path.abspath(os.path.dirname(__file__))
+        os.chdir(dir)
 
     @requests_mock.mock()
     def test_start_workflow(self, mock_request):
@@ -39,6 +46,30 @@ class TestUtils(unittest.TestCase):
             inputs_file2, options_file, url, user, password)
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result.headers.get('test'), 'header')
+
+    def test_download_to_map(self):
+        """Test download_to_map with local files to ensure it builds the map of paths to contents correctly"""
+        urls = ['data/a.txt', 'data/b.txt']
+        urls_to_content = cromwell_tools.download_to_map(urls)
+        self.assertIn('data/a.txt', urls_to_content)
+        self.assertIn('data/b.txt', urls_to_content)
+        self.assertEqual(urls_to_content['data/a.txt'], 'aaa\n')
+        self.assertEqual(urls_to_content['data/b.txt'], 'bbb\n')
+
+    def test_make_zip_in_memory(self):
+        """Test make_zip_in_memory produces an in-memory zip file with the expected contents"""
+        urls_to_content = {
+            'data/a.txt': 'aaa\n',
+            'data/b.txt': 'bbb\n'
+        }
+        bytes_buf = cromwell_tools.make_zip_in_memory(urls_to_content)
+        with zipfile.ZipFile(bytes_buf, 'r') as zf:
+            with zf.open('a.txt') as f1:
+                f1_contents = f1.read()
+            with zf.open('b.txt') as f2:
+                f2_contents = f2.read()
+        self.assertEqual(f1_contents, 'aaa\n')
+        self.assertEqual(f2_contents, 'bbb\n')
 
 
 if __name__ == '__main__':
