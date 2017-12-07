197388ef442912b5e759d29e9c04365fdb794bd2
Ambrose J. Carr <ambrosejcarr@gmail.com>
2017-12-06 20:37:43 -0500

    Added CLI, Combined tools from secondary-analysis

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
 from setuptools import setup
 
-setup(name='cromwell-tools',
-      version='1.0.0.dev1',
-      description='Utilities for interacting with the Cromwell workflow engine',
-      url='http://github.com/broadinstitute/cromwell-tools',
-      author='Mint Team',
-      author_email='mintteam@broadinstitute.org',
-      license='BSD 3-clause "New" or "Revised" License',
-      packages=['cromwell_tools'],
-      install_requires=[
-          'requests'
-      ],
-      include_package_data=True
-      )
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Natural Language :: English",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+
+
+setup(
+    name='cromwell-tools',
+    version='1.0.1',
+    description='Utilities for interacting with the Cromwell workflow engine',
+    classifiers=CLASSIFIERS,
+    url='http://github.com/broadinstitute/cromwell-tools',
+    author='Mint Team',
+    author_email='mintteam@broadinstitute.org',
+    license='BSD 3-clause "New" or "Revised" License',
+    packages=['cromwell_tools'],
+    install_requires=[
+        'requests',
+    ],
+    scripts=['cromwell_tools/scripts/cromwell-tools'],
+    include_package_data=True
+)
