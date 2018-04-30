deed7b2139e52707479807476b20423a010c43d7
Rex Wang <chengche@broadinstitute.org>
2018-04-30 15:36:08 -0400

    Fix bugs according to readthedocs build results.

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
