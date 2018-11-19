194cdc0c18baea0df23221afb2e978c074571a22
Dan Moran <danmoran@broadinstitute.org>
2018-11-19 10:58:17 -0500

    Address review comments.

--- cromwell_tools/cli.py
+++ cromwell_tools/cli.py
@@ -37,14 +37,14 @@ def parser(arguments=None):
         'secrets_file': 'Path to the JSON file containing username, password, and url fields.',
         'service_account_key': 'Path to the JSON key file for authenticating with CaaS.'
     }
+
+    def add_auth_args(subcommand_parser):
+        for arg_dest, help_text in auth_args.items():
+            subcommand_parser.add_argument('--{arg}'.format(arg=arg_dest.replace('_', '-')),
+                                           dest=arg_dest, default=None, type=str, help=help_text)
     # todo this should be a group which is called authentication
     for p in cromwell_sub_commands:
-        def add_arg(arg_dest, help_text):
-            p.add_argument('--%s' % arg_dest.replace('_', '-'), dest=arg_dest,
-                           default=None, type=str, help=help_text)
-
-        for arg, description in auth_args.items():
-            add_arg(arg, description)
+        add_auth_args(p)
 
     # submit arguments
     submit.add_argument('--wdl-file', dest='wdl_file', type=argparse.FileType('r'), required=True,
