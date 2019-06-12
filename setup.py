7df8689c60c32dc01f4a5387a1487360b331917f
Rex Wang <rexwangcc@gmail.com>
2019-06-12 14:37:05 -0400

    Make the classifiers more accurate.

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ CLASSIFIERS = [
     "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
 ]
